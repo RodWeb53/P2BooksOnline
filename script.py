8f2d5164b6d07703c430d8bcc8e15599333aaffa
Rodolphe R. CLEMENCEAU <RCLEMENCEAU@slts-asp.local>
2022-11-30 17:17:11 +0100

    Phase 2 mis en fonction et boucle pour le calcul du nombre de page par catégorie

--- script.py
+++ script.py
@@ -37,13 +37,13 @@ def produit(url):
         description = soup.find('p', {'class': ''}).text
 
 
-        print('poduct_page_url = ' + url + '\n\n' + 'universal_ product_code (upc) = ' + upc + '\n\n' + 'title = ' + titre + '\n\n'
-        + 'price_including_tax = ' + prix_ttc + '\n\n' + 'price_excluding_tax = ' + prix_ht + '\n\n' + 'number_available = ' + quantite_disponible
-        + '\n\n' + 'product_description = ' + description + '\n\n' + 'category = ' + categorie + '\n\n' + 'review_rating = ' + version
-        + '\n\n' + 'image_url = ' + image)
+        # print('poduct_page_url = ' + url + '\n\n' + 'universal_ product_code (upc) = ' + upc + '\n\n' + 'title = ' + titre + '\n\n'
+        # + 'price_including_tax = ' + prix_ttc + '\n\n' + 'price_excluding_tax = ' + prix_ht + '\n\n' + 'number_available = ' + quantite_disponible
+        # + '\n\n' + 'product_description = ' + description + '\n\n' + 'category = ' + categorie + '\n\n' + 'review_rating = ' + version
+        # + '\n\n' + 'image_url = ' + image)
 
     else:
-            print("Les arguments de l'url ne sont pas conforme")      
+            print("Les arguments de l'url ne sont pas conforme produit")      
      
 """ligne pour lancer la phase 1 du projet suite à la mise en place de la fonction produit """
 # lien = 'http://books.toscrape.com/catalogue/scott-pilgrims-precious-little-life-scott-pilgrim-1_987/index.html'
@@ -55,25 +55,44 @@ def produit(url):
 """Phase 2 du projet
 Récupérer les données des produits pour une catégorie"""
 
+def categorie_produit(categorie):
+    #Boucle pour connaitre le nombre de page par catégorie
+    nombre_page = 0
+    page_ok = True
 
-liens_categorie = []
+    while page_ok:
+        url_categorie = 'http://books.toscrape.com/catalogue/category/books/' + categorie + '/page-' + str(nombre_page + 1) + '.html'
+        page = requests.get(url_categorie)
+        if page.ok:
+            nombre_page = nombre_page + 1
+            print(nombre_page)
+        else:
+            page_ok = False    
 
-for i in range(1, 5):
-    """url pour le parcours des 4 pages"""
-    url_categorie = 'http://books.toscrape.com/catalogue/category/books/sequential-art_5/page-' + str(i) + '.html'
-    page = requests.get(url_categorie)
-    if page.ok:
-        soup = BeautifulSoup(page.content, 'html.parser')
 
-        """Récupération des liens par page et construction de l'adresse url"""
-        #creation de base_url pour enlever le chemin relatif récupéré
-        base_url = 'http://books.toscrape.com/catalogue'
-        liens_produits = soup.find_all('h3')
-        for lien_produit in liens_produits:
-            lien_virtuel = lien_produit.find('a')
-            lien_reel = lien_virtuel['href']
-            liens_categorie.append(base_url + lien_reel[8:])
-        print('les liens de la page  ' + str(i) + ' sont récupéré \n\n')
+
+    liens_categorie = []
+
+    for i in range(1, nombre_page + 1):
+        """url pour le parcours des 4 pages"""
+        url_categorie = 'http://books.toscrape.com/catalogue/category/books/' + categorie + '/page-' + str(i) + '.html'
+        page = requests.get(url_categorie)
+        if page.ok:
+            soup = BeautifulSoup(page.content, 'html.parser')
+
+            """Récupération des liens par page et construction de l'adresse url"""
+            #creation de base_url pour enlever le chemin relatif récupéré
+            base_url = 'http://books.toscrape.com/catalogue'
+            liens_produits = soup.find_all('h3')
+            for lien_produit in liens_produits:
+                lien_virtuel = lien_produit.find('a')
+                lien_reel = lien_virtuel['href']
+                liens_categorie.append(base_url + lien_reel[8:])
+            print('les liens de la page  ' + str(i) + ' sont récupéré \n\n')
+
+        else:
+            print("Les arguments de l'url ne sont pas conforme Categorie")
+
 
     """récpération des informations de chaque produit"""
     information_par_produit = []
@@ -83,8 +102,10 @@ for i in range(1, 5):
         information_par_produit.append(information_produit)
         print("le produit " + produit_categorie + " est recupéré  \n\n")
 
-    else:
-        print("Les arguments de l'url ne sont pas conforme") 
+
+
+
+categorie_produit('sequential-art_5')
 
 
 """!!! il reste le transfert vers un fichier csv à faire !!!""" 
